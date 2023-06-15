dd564951550c3dec044a560b5f78a8cbad3a97bd
sidee <said.issack@student.moringaschool.com>
2023-06-15 09:07:19 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1=31
+const num2=2
+let multiply = num1*num2
+
+let random = (Math.cell(Math.random()*10)+ 1)
+
+let num3 = 17
+let num4 = 13
+let mod = num3 % num4
+
+
+let max = (Math.max(15, 10, 12, 14, 17, 20, 7 , 9))
+
+
+  
+  
